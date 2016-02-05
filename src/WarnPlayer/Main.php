fc0afde9effa3dcda13aee529ee183ec2720c3f4
applqpak <applqpak@gmail.com>
2016-02-05 16:22:34 +1100

    Update Main.php

--- src/WarnPlayer/Main.php
+++ src/WarnPlayer/Main.php
@@ -15,6 +15,7 @@
       if($cmd->getName() === "warn") {
         if(!(isset($args[0]) and isset($args[1]))) {
           $sender->sendMessage(TF::RED . "Error: not enough args. Usage: /warn <player> <reason>");
+          return true;
         } else {
           $sender_name = $sender->getName();
           $player = $this->getServer()->getPlayer($args[0]);
@@ -24,6 +25,7 @@
           $player->sendMessage(TF::RED . "You have been warned by " . $sender_name . " for " . $reason . "!");
           $this->getServer()->broadcastMessage(TF::LIGHT_PURPLE . "PUNISHMENT >" . TF::YELLOW . $player_name . " was warned by " . $sender_name . " for " . $reason . "!");
           $sender->sendMessage(TF::GREEN . $player_name . " was warned for " . $reason . "!");
+          return true;
         }
       }
     }
